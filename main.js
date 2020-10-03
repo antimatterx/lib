b84cb07f3ca8bdb379466dbb49ccff9d7c2572e6
AntiMatter-X <51862109+AntiMatter-X@users.noreply.github.com>
2020-10-04 01:11:15 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 (function() {
     'use strict';
-    $("body").css("backgroundColor", "lightgray");
     var h = $("<div>").css({
-        backgroundColor: "darkgray",
+        backgroundColor: "lightgray",
         borderRadius: "25px",
         padding: "1em"
     }).appendTo("body");
