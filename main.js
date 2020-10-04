e78e57ec8855ea11d13cdb26343e152bb89ed4ba
AntiMatter-X <51862109+AntiMatter-X@users.noreply.github.com>
2020-10-04 16:53:54 +0900

    Update main.js

--- main.js
+++ main.js
@@ -6,5 +6,14 @@
         padding: "1em"
     }).appendTo("body");
     $("<h1>").text($("title").text()).appendTo(h);
-    h.append("<hr>Hello, World!");
+    h.append("<hr>Hello, World!<br>");
+    var tools = {
+        "test-page": "https://antimatter-x.github.io/lib/",
+        "JavaScript エディタ": "https://antimatter-x.github.io/editor/"
+    };
+    for (var k in tools) {
+        $("<a>", {
+            href: k
+        }).text(tools[k]).appendTo(h);
+    };
 })();
