d7faf321d6b2f29bbb28befab2d9bd1244a580f1
AntiMatter-X <51862109+AntiMatter-X@users.noreply.github.com>
2020-10-04 17:22:54 +0900

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@
         padding: "1em"
     }).appendTo("body");
     $("<h1>").text($("title").text()).appendTo(h);
-    var desc = $("<div>").text("Hello, World!").css({
+    var desc = $("<div>").text("Hello, World!\nテストページです。").css({
         backgroundColor: "darkgray",
         fontSize: "10px",
         borderRadius: "25px",
