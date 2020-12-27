fbbd5a88f51d8accbcc216265ee9128cc8407c90
AntiMatter-X <51862109+AntiMatter-X@users.noreply.github.com>
2020-12-27 20:50:41 +0900

    Update main.js

--- main.js
+++ main.js
@@ -43,7 +43,7 @@
         desc = page.desc,
         content = page.content;
     //--------------------------------------------------
-    $("<h2>").text("Hello, World!").css("color", "#f5f5f5").appendTo(desc);
+    $("<h2>").text("Hello, World!").appendTo(desc);
     $("<p>").html("テストページです。<br>どのライブラリが読み込めていないか確認できます。").appendTo(desc);
     //--------------------------------------------------
     $("<h2>").text("ライブラリのチェック").appendTo(content);
@@ -60,58 +60,103 @@
         });
     })($("<div>").css({
         padding: "10px",
-        width: "25%",
         color: "black",
+        display: "inline-block",
         backgroundColor: "rgba(255, 255, 255, 0.5)",
         borderRadius: "10px"
     }).appendTo(content));
     $("<h2>").text("簡易コンソール").appendTo(content);
-    (function(container) {
+    (function(container, clearAllInterval) {
         var input = $(amx.addInputText(container[0], {
                 textarea: true
             })).width("100%"),
             output = $("<div>").appendTo(container);
-        $("<button>").text("実行").css("margin-bottom", "5px").on("click", function() {
-            output.empty();
-            try {
-                console.log((0, eval)(input.val()));
-            } catch (e) {
-                console.error(e);
-            };
-        }).prependTo(container);
-
-        function displayLog(x, color) {
-            $("<div>").text((function() {
-                if (typeof x === "object") {
-                    if (x.message) return String(x);
-                    return "{" + Object.keys(x).map(function(k) {
-                        return k + ":" + String(x[k]);
-                    }).join(",") + "}";
-                } else return String(x);
-            })()).css("background-color", color).appendTo(output);
-        };
-        (function() {
-            if (!console) window.console = {};
-            console.log = function(x) {
-                displayLog(x, "gray");
-            };
-            console.error = function(x) {
-                displayLog(x, "pink");
-            };
-            console.warn = function(x) {
-                displayLog(x, "lightyellow");
-            };
-            console.info = function(x) {
-                displayLog(x, "lightblue");
-            };
-        })();
+        amx.addButton(container[0], {
+            title: "クリア",
+            insertBefore: true,
+            click: function() {
+                output.empty();
+            }
+        })
+        amx.addButton(container[0], {
+            title: "実行",
+            insertBefore: true,
+            click: function() {
+                output.empty();
+                try {
+                    console.log((0, eval)(input.val()));
+                } catch (e) {
+                    console.error(e);
+                };
+            }
+        });
+        (function(addResult, list) {
+            var origin = {};
+            if (!window.console) window.console = {};
+            amx.loop(list, function(k) {
+                origin[k] = window.console[k] || function() {};
+                var arr = list[k];
+                window.console[k] = (function() {
+                    var key = k,
+                        back = arr[0],
+                        color = arr[1],
+                        symbol = arr[2];
+                    return function() {
+                        origin[key].apply(console, arguments);
+                        addResult(amx.stringize(Array.prototype.slice.call(arguments), true), back, color, symbol);
+                    };
+                })();
+            });
+        })(function(str, back, color, symbol) {
+            var line = $("<div>").css({
+                    backgroundColor: back,
+                    color: color,
+                    textAlign: "left",
+                    maxWidth: "100%"
+                }).appendTo(output),
+                symbolColor = amx.getCSS(line[0]).backgroundColor.match(/[0-9]+/g).map(function(v, i) {
+                    var n = Number(v),
+                        d = (n - n * 0.1);
+                    return d >= 0 ? d : 0;
+                });
+            $("<div>").text(symbol || Array.prototype.slice.call(output.children()).filter(function(e) {
+                return e.style.backgroundColor === "white";
+            }).length - 1).css({
+                backgroundColor: "rgb(" + symbolColor + ")",
+                width: "3em",
+                textAlign: "center"
+            }).appendTo(line);
+            $("<div>").text(str).css("margin-left", "1em").appendTo(line);
+            //line.find("div").css("display", "inline-block");
+        }, { // back, color, symbol
+            log: ["white", "black", ""],
+            error: ["pink", "red", "×"],
+            warn: ["lightyellow", "orange", "▲"],
+            info: ["lightblue", "blue", "●"]
+        });
     })($("<div>").css({
         padding: "10px",
         color: "black",
         backgroundColor: "rgba(255, 255, 255, 0.5)",
         borderRadius: "10px"
-    }).appendTo(content));
-    content.append("<br>");
+    }).appendTo(content), (function() {
+        var setTimeout_copy = window.setTimeout,
+            setInterval_copy = window.setInterval;
+        var setIds = []; // 時間差関数のidを格納
+        window.setTimeout = function() {
+            var id = setTimeout_copy.apply(this, arguments);
+            setIds.push(id);
+            return id;
+        };
+        window.setInterval = function() {
+            var id = setInterval_copy.apply(this, arguments);
+            setIds.push(id);
+            return id;
+        };
+        return function() { // 時間差関数を全てクリアする
+            while (setIds.length) clearInterval(setIds.pop());
+        };
+    })());
     //--------------------------------------------------
     $("h2").css("color", "#f5f5f5");
     //--------------------------------------------------
